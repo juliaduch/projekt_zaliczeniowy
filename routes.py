4ca4436992e0eeef02becbf9335ad356e95fc105
Cynamoon224 <martyna.michalik@edu.uekat.pl>
2023-12-29 00:47:28 +0100

    Dodanie endpointow

--- routes.py
+++ routes.py
@@ -6,7 +6,7 @@ from models import Transakcje
 
 router = APIRouter()
 ## POST
-@router.post("/", response_description="Dodaj nowa transakcje", status_code=status.HTTP_201_CREATED, response_model=Transakcje)
+@router.post("/dodaj_transakcje", response_description="Dodaj nowa transakcje", status_code=status.HTTP_201_CREATED, response_model=Transakcje)
 def Dodaj_transakcje(request: Request, Transakcja: Transakcje = Body(...)):
     transakcja = jsonable_encoder(transakcja)
     nowa_transakcja = request.app.database["pythonproject"].insert_one(transakcja)
@@ -17,13 +17,13 @@ def Dodaj_transakcje(request: Request, Transakcja: Transakcje = Body(...)):
     return dodana_transakcja
 
 ## GET ALL
-@router.get("/", response_description="Wszystkie transakcje", response_model=List[Transakcje])
+@router.get("/transakcje", response_description="Wszystkie transakcje", response_model=List[Transakcje])
 def wszystkie_transakcje(request: Request):
     transakcje = list(request.app.database["pythonproject"].find(limit=100))
     return transakcje
 
 ## GET by ID
-@router.get("/{id}", response_description="Znajdz transakcje po id", response_model=Transakcje)
+@router.get("/transakcje/{id}", response_description="Znajdz transakcje po id", response_model=Transakcje)
 def znajdz_transakcje(id: str, request: Request):
     if (transakcja := request.app.database["pythonproject"].find_one({"_id": id})) is not None:
         return transakcja
